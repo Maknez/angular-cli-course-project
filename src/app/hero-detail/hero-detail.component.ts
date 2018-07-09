1a0700ec875fa08b5587f738fc22a1a617b51105
Maknez <themaknez@gmail.com>
2018-07-09 09:12:12 +0200

    7. HTTP +implement function to update hero values

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -22,6 +22,10 @@ export class HeroDetailComponent implements OnInit {
   goBack(): void {
     this.location.back();
   }
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
 
   constructor(
     private route: ActivatedRoute,
